46f7bcc43e858d8bda8b03d3a4282ecdbf904b95
Shivam <singhshivamkr@gmail.com>
2023-03-02 15:50:57 +0530

    feat: add build script

---
+++ build.js
@@ -0,0 +1,24 @@
+const { build } = require("esbuild");
+const { join } = require("path");
+
+// console.log(paths);
+
+console.log("Bundling lambdas to ESM modules");
+
+build({
+  absWorkingDir: process.cwd(),
+  bundle: true,
+  logLevel: "info",
+  entryPoints: ["./src/index.ts"],
+  outdir: join(process.cwd(), "./lib/"),
+  minify: true,
+  format: "esm",
+  target: "esnext",
+  platform: "node",
+  banner: {
+    js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+  },
+  outExtension: {
+    ".js": ".mjs",
+  },
+}).catch(() => process.exit(1));
